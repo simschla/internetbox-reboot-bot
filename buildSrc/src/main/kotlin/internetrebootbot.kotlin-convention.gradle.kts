866bb0e7440ce71073b4537526a8dc6f76961355
Simon Gamma <simon.gamma@ergon.ch>
2024-12-23 16:07:20 +0100

    chore(deps): update dependency ch.qos.logback:logback-classic to v1.5.15 (#94)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- buildSrc/src/main/kotlin/internetrebootbot.kotlin-convention.gradle.kts
+++ buildSrc/src/main/kotlin/internetrebootbot.kotlin-convention.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
 
     // logging
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.14")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.15")
 }
 
 kotlin {
